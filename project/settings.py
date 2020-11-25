77547dc44064cf98335024015e2eaf90629817fb
Bo Morin <morinbe@gmail.com>
2020-11-25 17:28:55 -0500

    Maybe this one works

--- project/settings.py
+++ project/settings.py
@@ -126,6 +126,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+STATIC_URL = '/borkle/static/'
 
 
 LOGIN_SUCCESS_REDIRECT = 'dashboard'
--- project/settings_local.py
+++ project/settings_local.py
@@ -1,6 +1,4 @@
 import os
 from project.settings import *  # noqa
 
-
-STATIC_URL = '/borkle/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'borkle/static')
